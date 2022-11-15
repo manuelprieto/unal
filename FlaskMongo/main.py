45f1b75d2f4cf71923c5a7fa72b3628bd9162a14
Manuel <manuelprietocolon@gmail.com>
2022-11-14 21:36:29 -0500

    vista y controlador partido

--- FlaskMongo/main.py
+++ FlaskMongo/main.py
@@ -2,8 +2,22 @@ from flask import Flask, request, Response
 from flask import jsonify
 from flask_cors import CORS
 
+from Controladores.PartidoControlador import PartidoControlador
+#from Controladores.CandidatoControlador import CandidatoControlador
+#from Controladores.MesaControlador import MesaControlador
+#from Controladores.ResultadoControlador import ResultadoControlador
+
 app = Flask(__name__)
 cors = CORS(app)
+
+
+##############################
+##     VARIABLES GLOBALES   ##
+##############################
+miControladorPartido = PartidoControlador()
+#miControladorCandidato = CandidatoControlador()
+#miControladorMesa = MesaControlador()
+#miControladorResultado = ResultadoControlador()
 
 ####################################
 ##    PROBAR EL SERVICIO          ##
@@ -12,6 +26,36 @@ cors = CORS(app)
 def test():
     json = {}
     json["message"] = "Server running ..."
+    return jsonify(json)
+
+#####################################
+##      ENDPOINT PARTIDOS          ##
+#####################################
+@app.route("/partidos", methods=["GET"])
+def getPartidos():
+    json = miControladorPartido.index()
+    return jsonify(json)
+
+@app.route("/partidos", methods=["POST"])
+def crearPartido():
+    data = request.get_json()
+    json = miControladorPartido.create(data)
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=["GET"])
+def getPartido(id):
+    json = miControladorPartido.show(id)
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=["PUT"])
+def modificarPartido(id):
+    data = request.get_json()
+    json = miControladorPartido.update(id, data)
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=["DELETE"])
+def eliminarPartido(id):
+    json = miControladorPartido.delete(id)
     return jsonify(json)
 
 if __name__ == "__main__":
