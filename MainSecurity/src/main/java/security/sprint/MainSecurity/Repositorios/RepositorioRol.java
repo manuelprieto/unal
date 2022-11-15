99910d5c83bc2ef460164c1afd6d389ae1d71337
Manuel <manuelprietocolon@gmail.com>
2022-11-15 18:02:25 -0500

    Repositorio + Backend Seguridad

--- MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioRol.java
+++ MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioRol.java
@@ -1,2 +1,7 @@
-package security.sprint.MainSecurity.Repositorios;public interface RepositorioRol {
+package security.sprint.MainSecurity.Repositorios;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import security.sprint.MainSecurity.Modelos.Rol;
+
+public interface RepositorioRol extends MongoRepository<Rol, String> {
 }
---
+++ MainSecurity/src/test/java/security/sprint/MainSecurity/MainSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package security.sprint.MainSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
