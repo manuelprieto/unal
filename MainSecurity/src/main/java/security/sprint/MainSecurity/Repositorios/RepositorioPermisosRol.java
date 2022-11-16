835b260586de5b0d9a070ac9846abce01fcc69c6
Manuel <manuelprietocolon@gmail.com>
2022-11-16 14:50:59 -0500

    Inicio Test

--- MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioPermisosRol.java
+++ MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioPermisosRol.java
@@ -1,2 +1,11 @@
-package security.sprint.MainSecurity.Repositorios;public interface RepositorioPermisosRol {
+package security.sprint.MainSecurity.Repositorios;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import security.sprint.MainSecurity.Modelos.PermisosRol;
+
+public interface RepositorioPermisosRol extends MongoRepository<PermisosRol,String> {
+    @Query("{'rol.$id':ObjectId(?0), 'permiso.$id':ObjectId(?1)}")
+    PermisosRol getPermisoRol(String id_rol, String id_permiso);
 }
+
