70bd77dd54ae0d32c0a87111244360f2299a1306
Manuel <manuelprietocolon@gmail.com>
2022-11-16 11:53:29 -0500

    Usuarios funcionando

--- MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioUsuario.java
+++ MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioUsuario.java
@@ -1,2 +1,11 @@
-package security.sprint.MainSecurity.Repositorios;public interface RepositorioUsuario {
+package security.sprint.MainSecurity.Repositorios;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import security.sprint.MainSecurity.Modelos.Usuario;
+
+public interface RepositorioUsuario extends MongoRepository<Usuario, String> {
+    @Query("{'correo':?0}")
+    Usuario getUserByMail(String correo);
 }
+
