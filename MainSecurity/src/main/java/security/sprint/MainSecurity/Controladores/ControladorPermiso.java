c4ab74bd69974fde2bd57994e51c4cedaf5e001e
Manuel <manuelprietocolon@gmail.com>
2022-11-16 14:16:47 -0500

    Controlador Permiso Repositorio

--- MainSecurity/src/main/java/security/sprint/MainSecurity/Controladores/ControladorPermiso.java
+++ MainSecurity/src/main/java/security/sprint/MainSecurity/Controladores/ControladorPermiso.java
@@ -1,2 +1,59 @@
-package security.sprint.MainSecurity.Controladores;public class ControladorPermiso {
+package security.sprint.MainSecurity.Controladores;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import security.sprint.MainSecurity.Modelos.Permiso;
+import security.sprint.MainSecurity.Repositorios.RepositorioPermiso;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/permisos")
+public class ControladorPermiso {
+    @Autowired
+    private RepositorioPermiso miRepositorioPermiso;
+
+    @GetMapping("")
+    public List<Permiso> index(){
+        return miRepositorioPermiso.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Permiso create(@RequestBody Permiso infoPermiso){
+        return miRepositorioPermiso.save(infoPermiso);
+    }
+
+    @GetMapping("{id}")
+    public Permiso show(@PathVariable String id){
+        Permiso permisoActual= miRepositorioPermiso.findById(id)
+                .orElse(null);
+        return permisoActual;
+    }
+
+    @PutMapping("{id}")
+    public Permiso update(@PathVariable String id,
+                          @RequestBody Permiso infoPermiso){
+
+        Permiso permisoActual= miRepositorioPermiso.findById(id)
+                .orElse(null);
+        if(permisoActual!=null){
+            permisoActual.setUrl(infoPermiso.getUrl());
+            permisoActual.setMetodo(infoPermiso.getMetodo());
+            return miRepositorioPermiso.save(permisoActual);
+        }else{
+            return null;
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Permiso permisoActual= miRepositorioPermiso.findById(id)
+                .orElse(null);
+        if(permisoActual!=null){
+            miRepositorioPermiso.delete(permisoActual);
+        }
+    }
 }
--- MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioPermiso.java
+++ MainSecurity/src/main/java/security/sprint/MainSecurity/Repositorios/RepositorioPermiso.java
@@ -1,2 +1,9 @@
-package security.sprint.MainSecurity.Repositorios;public interface RepositorioPermiso {
+package security.sprint.MainSecurity.Repositorios;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import security.sprint.MainSecurity.Modelos.Permiso;
+
+public interface RepositorioPermiso extends MongoRepository<Permiso, String> {
+    @Query("{'url':?0, 'metodo':?1}")
+    Permiso getPermiso(String url, String metodo);
 }
